3d98fd0fa3014a58e7abefe86e780a35eda21942
VaibhaV <vaibhav.codes09@gmail.com>
2026-01-19 11:34:42 +0530

    Move firebase config to env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,13 +7,14 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDqxR1js2w8wnZ3It9KOhrxVRedXFPR0I4",
-  authDomain: "job-tracker-f93a1.firebaseapp.com",
-  projectId: "job-tracker-f93a1",
-  storageBucket: "job-tracker-f93a1.firebasestorage.app",
-  messagingSenderId: "575791628350",
-  appId: "1:575791628350:web:5bbe94920e0ecca0acd16f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
